e6f79a9d80158b84cf47753904489b3d1cc0bc2f
Saptak Bhattacharyya <saptakbhattacharyya16@gmail.com>
2026-04-13 13:32:17 +0530

    Time: 0 ms (100%), Space: 11.9 MB (83.42%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+
+        unordered_map<int,int> m;
+        unordered_set<int> s;
+        for(int i : arr){
+            m[i]++;
+        }
+        for(auto i : m){
+            if(s.find(i.second) != s.end())
+                return false;
+            s.insert(i.second);
+        }
+        return true;
+    }
+};
