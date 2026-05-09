9a38f748f93d1743381fbeda41f2c745aa126256
Saptak Bhattacharyya <saptakbhattacharyya16@gmail.com>
2026-05-09 13:18:05 +0530

    Time: 14 ms (12.69%), Space: 13.4 MB (18.29%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int romanToInt(string s) {
-       unordered_map<char, int> romanValues = {
-            {'I', 1}, {'V', 5}, {'X', 10},
-            {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
-        };
+      unordered_map<char, int> romanValues = {
+    {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, 
+    {'C', 100}, {'D', 500}, {'M', 1000}
+};
+        
         int total = 0;
         int n = s.length();
         for (int i = 0; i < n; i++) {
@@ -15,6 +16,6 @@ public:
                 total += romanValues[s[i]];
             }
         }
-        return total;
+     return total;
     }
 };
