a4228f49507a117430d91213034ee934f8e2a029
Saptak Bhattacharyya <saptakbhattacharyya16@gmail.com>
2026-05-21 02:45:39 +0530

    Time: 0 ms (100%), Space: 9.1 MB (15.27%) - LeetHub

--- 3019-number-of-changing-keys/3019-number-of-changing-keys.cpp
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int countKeyChanges(string s) {
-        int count = 0;
-        for (int i = 0; i < s.size() - 1; ++i) {
-            if (tolower(s[i]) != tolower(s[i + 1])) {
-                count++;
-            }
+       int count=0;
+       for(int i=0;i<s.size()-1;i++){
+        if(tolower(s[i])!=tolower(s[i+1])){
+        count ++;
         }
-        return count;
+       }
+       return count;
     }
 };
