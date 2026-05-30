02f6aa33a8fc3552eafe2d15d9a9667f3b5ace03
Saptak Bhattacharyya <saptakbhattacharyya16@gmail.com>
2026-05-31 04:43:03 +0530

    Time: 7 ms (15%), Space: 15.8 MB (36.83%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int findFinalValue(vector<int>& nums, int original) {
-        unordered_map<int, bool> m;
-        for (int n : nums) m[n] = true;
-        while (m.count(original)) original *= 2;
-        return original;
+       unordered_map<int,bool>m;
+       for(int n : nums){
+        m[n]=true;
+       }
+       while(m.count(original)){
+        original*=2;
+       }
+       return original;
     }
 };
