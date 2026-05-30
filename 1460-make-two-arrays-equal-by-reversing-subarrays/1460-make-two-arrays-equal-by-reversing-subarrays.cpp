d22092241d091acae840c08f85366012b2dc40a0
Saptak Bhattacharyya <saptakbhattacharyya16@gmail.com>
2026-05-31 01:10:03 +0530

    Time: 11 ms (5.59%), Space: 22.1 MB (5.42%) - LeetHub

---
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        multiset<int> ms1(target.begin(), target.end());
+        multiset<int> ms2(arr.begin(), arr.end());
+        return ms1 == ms2;
+    }
+};
