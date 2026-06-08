e438323e899836daf2aed9f685fd97c416621e60
Saptak Bhattacharyya <saptakbhattacharyya16@gmail.com>
2026-06-08 11:57:13 +0530

    Time: 1607 ms (5.04%), Space: 74.1 MB (93.61%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-           unordered_set<int>seen;
-           for(int i=0;i<nums.size();i++){
-           if(i>k){
-            seen.erase(nums[i-k-1]);
-           }
-           if(seen.find(nums[i])!=seen.end()){
-            return true;
-           }
-           seen.insert(nums[i]);
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j <= i + k && j < n; j++) {
+                if (nums[i] == nums[j]) {
+                    return true; 
+                }
+            }
         }
         return false;
     }
