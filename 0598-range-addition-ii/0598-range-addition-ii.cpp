2b9cf9e41f595bebf869ccb1f2f982f891455e5b
Saptak Bhattacharyya <saptakbhattacharyya16@gmail.com>
2026-06-19 01:49:30 +0530

    Time: 0 ms (100%), Space: 15.8 MB (12%) - LeetHub

---
+++ 0598-range-addition-ii/0598-range-addition-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxCount(int m, int n, vector<vector<int>>& ops) {
+         int minX = m;
+        int minY = n;
+        for (auto v : ops)
+        {
+            minX = min(minX, v[0]);
+            minY = min(minY, v[1]);
+        }
+        return minX*minY;
+    }
+};
